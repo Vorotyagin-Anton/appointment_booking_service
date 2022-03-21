8b0dd364c7909b0fa574f0b15d235979787112e9
Evgeny Kuzmin <skchel174@gmail.com>
2022-03-21 21:32:43 +0500

    Added application styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,3 +9,16 @@ export default defineComponent({
   name: 'App'
 })
 </script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    color: $white;
+    text-decoration: none;
+  }
+</style>
