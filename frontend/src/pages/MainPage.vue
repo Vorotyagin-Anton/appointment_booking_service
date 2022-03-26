9889571f51b17cfc04db0f7f5f42f15cbca274d0
Evgeny Kuzmin <skchel174@gmail.com>
2022-03-26 18:47:01 +0500

    Added CatalogTabs component to MainPage

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,18 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     <slider-promo/>
+    <catalog-tabs/>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import SliderPromo from "components/SliderPromo";
+import CatalogTabs from "components/Catalog/CatalogTabs";
 
 export default defineComponent({
   name: 'IndexPage',
 
   components: {
     SliderPromo,
+    CatalogTabs,
   }
 })
 </script>
