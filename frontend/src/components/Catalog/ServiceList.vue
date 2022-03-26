0680049c2286633a859686cfbcb0cda3d96b2cbe
Evgeny Kuzmin <skchel174@gmail.com>
2022-03-26 18:46:14 +0500

    Created ServiceList component

---
+++ frontend/src/components/Catalog/ServiceList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="app-services">
+    <div class="app-services__items" v-if="isServicesLoaded">
+      <service-list-item
+        v-for="item in serviceList"
+        :item="item"
+        :key="item.id"
+      />
+    </div>
+    <div v-else class="app-services__loading">
+      <q-circular-progress
+        reverse
+        indeterminate
+        size="50px"
+        color="light-blue"
+        class="q-ma-md"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import useServiceList from "src/hooks/useServiceList";
+import ServiceListItem from "components/Catalog/ServiceListItem";
+
+export default defineComponent({
+  name: "ServiceList",
+
+  components: {
+    ServiceListItem,
+  },
+
+  setup() {
+    const {serviceList, isServicesLoaded} = useServiceList();
+    return {
+      serviceList,
+      isServicesLoaded,
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.app-services {
+  height: 100%;
+  padding: 25px 50px;
+
+  &__items {
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+  }
+
+  &__loading {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
---
+++ frontend/src/components/Catalog/ServiceListItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-card class="service-card">
+    <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <div class="absolute-bottom text-h6">
+        {{ title }}
+      </div>
+    </q-img>
+
+    <q-card-section>{{ content }}</q-card-section>
+  </q-card>
+</template>
+
+<script>
+import {defineComponent, toRefs} from "vue";
+
+export default defineComponent({
+  name: "ServiceListItem",
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const { item } = toRefs(props);
+
+    return {
+      title: item.value.title,
+      content: item.value.content,
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.service-card {
+  width: 100%;
+  max-width: 280px;
+  margin-bottom: 35px;
+  margin-right: 35px;
+  transition: 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.04);
+    cursor: pointer;
+  }
+}
+</style>
