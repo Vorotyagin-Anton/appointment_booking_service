90aad424e6a9f4375a43037f6a8496358ce226d1
VorotyaginAnton <a.vorotyagin@yandex.ru>
2022-03-27 21:16:25 +0300

    feat(api): add workers route

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -11,9 +13,41 @@ use Symfony\Component\Serializer\SerializerInterface;
 class UserController extends AbstractController
 {
     #[Route('/api/user', name: 'app_user')]
-    public function index(UserRepository $userRepository, SerializerInterface $serializer): Response
+    public function getAllUsers(UserRepository $userRepository, SerializerInterface $serializer): Response
     {
         $users = $userRepository->findAll();
         return $this->json($serializer->serialize($users, 'json'));
+    }
+
+    #[Route('/api/workers', name: 'app_masters')]
+    public function getMastersOnlyByPages(
+        UserRepository $userRepository,
+        SerializerInterface $serializer,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response
+    {
+        $itemsPerPage = $request->query->getInt('itemsPerPage', 3);
+        $pageNumber = $request->query->getInt('pageNumber', 1);
+
+        $pagination = $paginator->paginate(
+            $userRepository->getWorkersOnlyQueryBuilder(),
+            $pageNumber,
+            $itemsPerPage
+        );
+
+        $totalItems = $pagination->getTotalItemCount();
+        $totalPages = ceil($totalItems/$itemsPerPage);
+
+        $result = [
+            'items' => $pagination,
+            'currentPageNumber' => $pageNumber,
+            'itemsPerPage' => $itemsPerPage,
+            'totalItems' => $totalItems,
+            'totalPages' => $totalPages
+        ];
+
+
+        return $this->json($serializer->serialize($result, 'json'));
     }
 }
