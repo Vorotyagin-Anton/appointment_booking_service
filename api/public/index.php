3cdc39ecbf66f65168b7bcc333f5db47c9f4d1f3
VorotyaginAnton <a.vorotyagin@yandex.ru>
2022-04-05 21:54:07 +0300

    feat(api): add headers for cors

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,11 @@
 
 use App\Kernel;
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Allow: GET, POST, PUT, PATCH, DELETE");
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
