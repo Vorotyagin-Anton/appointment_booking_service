a3aa27c05452970219ef10875b28b575baef370b
VorotyaginAnton <a.vorotyagin@yandex.ru>
2022-04-09 20:06:25 +0300

    fix(api): allow PATCH; change photo path in fixtures

--- api/public/index.php
+++ api/public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
 header("Allow: GET, POST, PUT, PATCH, DELETE");
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
