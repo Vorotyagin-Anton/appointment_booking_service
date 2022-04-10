2ab6f0558ba33ee5ec2fc2b19ce2d0b76492caa5
VorotyaginAnton <a.vorotyagin@yandex.ru>
2022-04-10 22:22:54 +0300

    fix(api): delete custom headers

--- api/public/index.php
+++ api/public/index.php
@@ -2,11 +2,6 @@
 
 use App\Kernel;
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
-header("Allow: GET, POST, PUT, PATCH, DELETE");
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
