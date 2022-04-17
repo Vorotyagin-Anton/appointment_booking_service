89e7a36cb80a5c15c7e0ad0c8913bf8673c500fc
VorotyaginAnton <a.vorotyagin@yandex.ru>
2022-04-17 20:12:26 +0300

    feat(api): add verify-email bundle, form, validator

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
