dce71387e07013553518db1f02046eda0c49ae7f
Evgeny Kuzmin <skchel174@gmail.com>
2022-05-07 23:40:58 +0500

    Created ConnectSection

---
+++ frontend/src/components/Sections/ConnectSectionOption.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="connect-option">
+    <span class="material-icons connect-option__icon">{{ icon }}</span>
+    <h3 class="connect-option__title">{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConnectSectionOption",
+  
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.connect-option {
+  display: flex;
+  flex-direction: column;
+  padding: 25px 60px;
+  width: 320px;
+  height: 210px;
+  transition-property: box-shadow;
+  transition-duration: .5s;
+  box-shadow: rgba(100, 100, 111, .2) 0 7px 29px 0;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, .16) 0 10px 36px 0, rgba(0, 0, 0, .06) 0 0 0 1px;
+  }
+
+  &__icon {
+    margin-bottom: 25px;
+    font-size: 40px;
+    color: $blue-500;
+  }
+
+  &__title {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 500;
+    line-height: 30px;
+    letter-spacing: normal;
+  }
+}
+</style>
--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -4,6 +4,7 @@
     <catalog-tabs/>
     <featured-masters/>
     <sign-up-invitation/>
+    <connect-section/>
     <faq-component/>
   </q-page>
 </template>
@@ -13,6 +14,7 @@ import PromoSection from "components/Sections/PromoSection";
 import CatalogTabs from "components/Catalog/CatalogTabs";
 import FeaturedMasters from "components/Sections/FeaturedMasters";
 import SignUpInvitation from "components/Sections/SignUpInvitation";
+import ConnectSection from "components/Sections/ConnectSection";
 import FaqComponent from "components/Sections/FaqSection";
 
 export default {
@@ -23,6 +25,7 @@ export default {
     CatalogTabs,
     FeaturedMasters,
     SignUpInvitation,
+    ConnectSection,
     FaqComponent,
   },
 }
