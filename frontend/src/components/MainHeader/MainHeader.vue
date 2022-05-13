eb49e2b8b489900bb79c10494e318cb88846c20c
Evgeny Kuzmin <skchel174@gmail.com>
2022-05-13 16:38:58 +0500

    Added account link to header

--- frontend/src/components/MainHeader/MainHeader.vue
+++ frontend/src/components/MainHeader/MainHeader.vue
@@ -2,17 +2,56 @@
   <q-header reveal class="container main-header">
     <div class="main-header__row">
       <q-toolbar class="main-header__col">
-        <q-toolbar-title class="main-header__title">Booking Service</q-toolbar-title>
+        <router-link :to="{name: 'main'}">
+          <q-toolbar-title class="main-header__title">
+            Booking Service
+          </q-toolbar-title>
+        </router-link>
       </q-toolbar>
 
-      <div class="main-header__col main-header__menu">
-
-      </div>
-
       <div class="main-header__col">
-        <q-btn class="main-header__signin" flat>
+        <q-btn
+          class="main-header__btn main-header__account"
+          v-if="isAuthorized"
+          label="My Account"
+          flat
+        >
+          <q-menu
+            class="main-header__dropdown"
+            anchor="bottom right"
+            self="top right"
+          >
+            <q-list style="min-width: 100px">
+              <q-item clickable v-close-popup dense>
+                <q-item-section>
+                  <router-link class="main-header__link" :to="{name: 'profile'}">Profile</router-link>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup dense>
+                <q-item-section>
+                  <router-link class="main-header__link" to="/">Dashboard</router-link>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup dense>
+                <q-item-section>
+                  <router-link class="main-header__link" to="/">Support</router-link>
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-close-popup dense>
+                <q-item-section>
+                  <router-link class="main-header__link" to="/">Sign Out</router-link>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
+
+        <q-btn v-else flat>
           <router-link
-            class="main-header__signin"
+            class="main-header__btn main-header__signin"
             :to="{name: 'signin'}"
           >
             Sign In
@@ -24,11 +63,19 @@
 </template>
 
 <script>
-import {defineComponent} from 'vue';
+import useAuth from "src/hooks/auth/useAuth";
 
-export default defineComponent({
+export default {
   name: "MainHeader",
-})
+
+  setup() {
+    const {isAuthorized} = useAuth();
+
+    return {
+      isAuthorized,
+    }
+  },
+}
 </script>
 
 <style lang="scss">
@@ -52,6 +99,8 @@ export default defineComponent({
   &__title {
     display: flex;
     align-items: center;
+    color: $black;
+    font-size: 26px;
   }
 
   &__icon {
@@ -59,15 +108,35 @@ export default defineComponent({
     margin-right: 10px;
   }
 
-  &__signin {
+  &__btn {
     display: flex;
     justify-content: center;
     align-items: center;
+    text-transform: capitalize;
+    color: $black;
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  &__signin {
     width: 80px;
     height: 40px;
-    text-transform: capitalize;
-    color: $dark;
-    font-size: 16px;
+  }
+
+  &__account {
+    width: 140px;
+    height: 50px;
+  }
+
+  &__dropdown {
+    width: 250px;
+    padding: 15px 0;
+  }
+
+  &__link {
+    padding: 5px 15px;
+    color: $black;
+    font-size: 15px;
     font-weight: 500;
   }
 }
