688f218ac711cb1f36449c07028a5824d25819df
Evgeny Kuzmin <skchel174@gmail.com>
2022-09-06 19:08:52 +0500

    Created CalendarBanner

---
+++ frontend/src/components/Cabinet/Banners/Calendar/SalesItem.vue
@@ -0,0 +1,58 @@
+<script setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+
+  value: {
+    type: [String, Number],
+    required: true,
+  },
+
+  route: {
+    type: String,
+    default: null,
+  },
+});
+
+const handleCLick = () => router.push(props.route ? {name: props.route} : {path: '#'});
+</script>
+
+<template>
+  <div class="sales-item" @click="handleCLick">
+    <div class="sales-item__label">{{ label }}</div>
+    <div class="sales-item__value">{{ value }}</div>
+  </div>
+</template>
+
+<style lang="scss">
+.sales-item {
+  width: 100%;
+  min-width: 65px;
+  max-width: 225px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover > .sales-item__label {
+    color: $primary;
+  }
+
+  &__label {
+    color: $grey-7;
+    font-size: 13px;
+  }
+
+  &__value {
+    color: $dark;
+    font-size: 15px;
+    font-weight: 500;
+  }
+}
+</style>
--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -1,5 +1,5 @@
 import moment from "moment/moment";
 
 export function formatSlotTime(date, minutes) {
-  return moment(date).add(minutes, 'minutes').format("HH:mm DD-MM-YYYY");
+  return moment(date).add(minutes, 'minutes').format("YYYY-MM-DD HH:mm");
 }
